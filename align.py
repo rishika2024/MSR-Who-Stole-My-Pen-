0125e34c7a90abeb589ab4df70448d8a79ba87d5
rishika2024 <berarishika@gmail.com>
2025-09-12 02:04:05 -0500

    #basic class structuring done

---
+++ align.py
@@ -0,0 +1,116 @@
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+
+class Alignment:
+    def __init__(self, clipping_distance_in_meters = 1, fps = 30):
+        
+        self.pipeline = rs.pipeline()
+        self.config = rs.config()
+
+        self.pipeline_wrapper = rs.pipeline_wrapper(self.pipeline)
+        self.pipeline_profile = self.config.resolve(self.pipeline_wrapper)
+        self.device = self.pipeline_profile.get_device()
+        self.device_product_line = str(self.device.get_info(rs.camera_info.product_line))
+
+        found_rgb = False  
+        for s in self.device.sensors: 
+            if s.get_info(rs.camera_info.name) == 'RGB Camera':
+                found_rgb = True
+                break
+        if not found_rgb:
+            print("The demo requires Depth camera with Color sensor")
+            exit(0)
+
+        self.fps = fps
+
+        self.config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, self.fps)
+        self.config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, self.fps)
+
+        # Start streaming
+        self.profile = self.pipeline.start(self.config)
+
+        # Getting the depth sensor's depth scale (see rs-align example for explanation)
+        self.depth_sensor = self.profile.get_device().first_depth_sensor()
+        self.depth_scale = self.depth_sensor.get_depth_scale()
+        #print("Depth Scale is: " , self.depth_scale)
+
+        #We will be removing  the background of objects more than
+        #clipping_distance_in_meters meters away
+        self.clipping_distance_in_meters = clipping_distance_in_meters #1 meter
+        self.clipping_distance = self.clipping_distance_in_meters / self.depth_scale
+
+        # Create an align object
+        # rs.align allows us to perform alignment of depth frames to others frames
+        # The "align_to" is the stream type to which we plan to align depth frames.
+        self.align_to = rs.stream.color
+        self.align = rs.align(self.align_to)
+
+    def aligned_frames(self):
+        # Get frameset of color and depth
+        frames = self.pipeline.wait_for_frames()
+        # frames.get_depth_frame() is a 640x360 depth image
+
+        # Align the depth frame to color frame
+        aligned_frames = self.align.process(frames)
+
+        # Get aligned frames
+        aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+        color_frame = aligned_frames.get_color_frame()
+
+        #   Validate that both frames are valid
+        if not aligned_depth_frame or not color_frame:
+            return None, None            
+
+        depth_image = np.asanyarray(aligned_depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        return depth_image, color_image
+        
+    def remove_bg(self, depth_image, color_image, bg_color = 153):
+        # Remove background - Set pixels further than clipping_distance to grey
+            
+        depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
+        bg_removed = np.where((depth_image_3d > self.clipping_distance) | (depth_image_3d <= 0), bg_color, color_image)
+
+        return bg_removed
+        
+    def render(self, bg_removed, depth_image):
+        # Render images:
+        #depth align to color on left
+        #depth on right
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        images = np.hstack((bg_removed, depth_colormap))
+        return images
+        
+    def stop(self):
+        self.pipeline.stop()
+
+if __name__ == "__main__":
+
+    align = Alignment(clipping_distance_in_meters=1, fps=30)
+
+    try:
+        while True:
+            depth_image, color_image = align.aligned_frames()
+            bg_removed = align.remove_bg(depth_image, color_image, bg_color = 153)
+            images = align.render(bg_removed, depth_image)
+
+            cv2.namedWindow('Align Example', cv2.WINDOW_NORMAL)
+            cv2.imshow('Align Example', images)
+            key = cv2.waitKey(1)
+            # Press esc or 'q' to close the image window
+            if key & 0xFF == ord('q') or key == 27:
+                cv2.destroyAllWindows()
+                break
+    finally:
+        align.stop()
+
+            
+        
+    
+
+
+
+    
+
